8c28dce9c79ee43de4cf7cadd85995e231f3ab3a
Svetlana Semenova <lampchkailicha@rambler.ru>
2022-01-23 20:31:21 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int priceTicket = 3000;
+        int moneyForOneBonusMile = 20;
+        int bonus = priceTicket / moneyForOneBonusMile;
+        System.out.println("Ваш бонус составляет: " + bonus);
+    }
+}
